c38b875552449600788d9aba175820e5232f7f1f
me <me@stuffihavemade.com>
2011-07-23 14:39:09 -0400

    Implemented changes to implementation so that tests pass. In particular, the implementation of responds_to? so it that it checks super if responds first, instead of checking last after all the demeter methods have been checked.

--- lib/demeter.rb
+++ lib/demeter.rb
@@ -5,10 +5,11 @@ module Demeter
       extend ClassMethods
 
       class << self
-        attr_accessor :demeter_names
+        attr_accessor :demeter_names, :demeter_default_child
       end
 
       base.demeter_names = []
+      base.demeter_default_child = nil
     end
   end
 
@@ -17,7 +18,14 @@ module Demeter
       object_method_name = method_name.to_s
       object_name = self.class.demeter_names.find {|name| object_method_name =~ /^#{name}_/ }
 
-      return super unless object_name
+      if object_name.nil?
+        child = self.class.demeter_default_child
+        if (not child.nil?) and ((self.send child).respond_to? method_name)
+          return (self.send child).send *([method_name] + attrs)
+        else
+          return super
+        end
+      end
 
       object_method_name.gsub!(/^#{object_name}_/, "")
 
@@ -30,30 +38,106 @@ module Demeter
     end
 
     def respond_to?(method_name, include_private = false)
+      if super
+        return true
+      end
+
       object_method_name = method_name.to_s
       object_name = self.class.demeter_names.find {|name| object_method_name =~ /^#{name}_/ }
+
 
       if object_name && (object = send(object_name))
         object.respond_to?(object_method_name.gsub(/^#{Regexp.escape(object_name.to_s)}_/, ""))
       else
-        super
+        child = self.class.demeter_default_child
+        if not child.nil?
+          (self.send child).respond_to? method_name
+        else
+          false
+        end
       end
     end
   end
 
   module ClassMethods
-    def demeter(*attrs)
-      if attrs.inject(true){|x,y| x and y.is_a? Symbol}
-        self.demeter_names = attrs
-        self.class_eval do
-          attrs.each do |name|
-            attr_accessor name
+    def eval_hash hash, to_be_demetered
+      hash.each do |child_object_name, options|
+        to_be_demetered << child_object_name
+        inner_options = get_inner_options options
+        eval_possible_default_option inner_options, child_object_name
+        eval_possible_delegators inner_options, child_object_name
+      end
+    end
+
+    def eval_possible_default_option options, child_object_name
+      if options.include? :default
+        raise TooManyDefaultsError unless self.demeter_default_child.nil?
+        self.demeter_default_child = child_object_name
+      end
+    end
+
+    def eval_possible_delegators options, child_object_name
+      if has_delegators? options
+        delegators = get_delegators options
+        delegators[:delegate].each do |new_method_name, old_method_name|
+          self.class_eval do
+            full_new_name = child_object_name.to_s + '_' + new_method_name.to_s
+            full_old_name = child_object_name.to_s + '_' + old_method_name.to_s
+            define_method(full_new_name) do |*args|
+              self.send *([full_old_name] + args)
+            end
           end
+        end
+      end
+    end
+
+    def object_name_only? attr
+      (not attr.respond_to? :values) or (not attr.respond_to? :keys)
+    end
+
+    def has_delegators? options
+      not get_delegators(options).nil?
+    end
+
+    def get_delegators options
+      delegators = options.find do |d|
+        d.respond_to? :keys and not d[:delegate].nil?
+      end 
+    end
+
+    def get_inner_options options
+      if many_options? options
+        options
+      else
+        [options]
+      end
+    end
+
+    def many_options? option_value
+      option_value.respond_to? :each
+    end
+
+    def demeter(*attrs)
+      to_be_demetered = []
+      attrs.each do |a|
+        if object_name_only? a
+          to_be_demetered << a 
+        else
+          eval_hash a, to_be_demetered
+        end
+      end
+      self.demeter_names = to_be_demetered
+      self.class_eval do
+        demeter_names.each do |name|
+          attr_accessor name
         end
       end
     end
   end
 end
 
-# ActiveRecord support
-require "demeter/active_record"
+  # ActiveRecord support
+  require "demeter/active_record"
+
+  class TooManyDefaultsError < Exception
+  end
